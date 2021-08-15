586848e86825b479e00f9d86108a990f33e81a8b
HarunDyn <harundayann@gmail.com>
2021-08-15 18:05:10 +0300

    lesson2-variables

---
+++ udemy_bootcamp/variables.js
@@ -0,0 +1,8 @@
+var whopper= 'hamburger'
+console.log(whopper)
+whopper=10
+console.log(whopper)
+console.log(typeof(whopper))
+var abonemi= false
+console.log(abonemi)
+console.log(typeof(abonemi))
