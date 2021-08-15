01568a8279b2ca22e54e9a376289b88cb30d440e
HarunDyn <harundayann@gmail.com>
2021-08-15 18:09:17 +0300

    lesson3-loop

---
+++ udemy_bootcamp/loop.js
@@ -0,0 +1,10 @@
+function sayiUret(k = 30) {
+    return Math.ceil(Math.random() * k)
+    
+    
+}
+for(var  i=1; i<=6; i++){ 
+    console.log(i + '.KONSOL') ; 
+    for(var j=1; j<=8; j++){console.log(sayiUret())}}
+
+
---
+++ udemy_bootcamp/while.js
@@ -0,0 +1,4 @@
+var sayi=1
+while(sayi<=10){ console.log(sayi)
+ sayi=sayi+1
+}
