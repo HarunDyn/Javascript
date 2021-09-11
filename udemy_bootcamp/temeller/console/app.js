a79d71d9d94bb7012a25c29698f1b75dd3dc372c
HarunDyn <harundayann@gmail.com>
2021-09-11 23:29:24 +0300

    console*

---
+++ udemy_bootcamp/temeller/console/app.js
@@ -0,0 +1,11 @@
+// console.log('Hoşgeldiniz')
+// console.log(234325)
+// console.log(true)
+// console.log(false)
+// console.log('tamam')
+
+console.warn('Bu bir uyarıdır.')
+console.error('Bu sayfa bulunamadı')
+// console.clear();
+console.log(true)
+console.log('true')
