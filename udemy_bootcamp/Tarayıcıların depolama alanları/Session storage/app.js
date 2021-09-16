4c5d4488b1de8b4343472286275a4507e2ba241f
HarunDyn <harundayann@gmail.com>
2021-09-17 01:05:24 +0300

    session_storage

---
+++ udemy_bootcamp/Tarayıcıların depolama alanları/Session storage/app.js
@@ -0,0 +1,39 @@
+// Session Storage  Oturum açık olduğu sürece depolama alanı oluşturur
+
+// button
+// const add=document.querySelector('#add');
+// const del=document.querySelector('#delete');
+// const clear=document.querySelector('#clear');
+
+// // İnputlar
+
+// const addkey=document.querySelector('#addkey');
+// const addvalue=document.querySelector('#addvalue');
+// const deletekey=document.querySelector('#deletekey');
+
+// add.addEventListener('click',additem);
+// function additem(e){
+//     sessionStorage.setItem(addkey.value,addvalue.value);
+
+// };
+
+// del.addEventListener('click',delitem);
+// function delitem(e){
+//     sessionStorage.removeItem(deletekey.value);
+
+// };
+
+// clear.addEventListener('click',clearitem);
+// function clearitem(e){
+//     sessionStorage.clear();
+
+// };
+
+sessionStorage.setItem('hareket','burpee');
+sessionStorage.clear();
+
+
+
+
+
+
